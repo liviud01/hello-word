cc3ec8c24fcbbb0d8d7452fb09f01a726dbd96ab
liviudarie01 <70706363+liviudarie01@users.noreply.github.com>
2020-09-14 13:37:11 -0400

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -4,6 +4,10 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a GitHub test");
+		if(args.length > 1) {
+			System.out.println("This is GitHub test with more params");
+		} else {
+			System.out.println("This is GitHub original test");
+		}		
 	}
 }
