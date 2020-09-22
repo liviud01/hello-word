d7510aadbae4510fffc8438136c0c3f392b73ff5
MyName <MyName@gmail.com>
2020-09-21 23:06:32 -0400

    push muy local changes

--- MyTest.java
+++ MyTest.java
@@ -4,10 +4,8 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("This is a GitHub test modified locally");
-=======
-		System.out.println("This is a GitHub test changed on master");
->>>>>>> ac833b3cc2a26afd6577f8c51ed8fa843215b4c5
+
 	}
 }
